8fdcd62f5dd09d8ade506f573db0c52e8b22d34d
Syed Aman <sna274@nyu.edu>
2017-10-18 06:40:53 -0500

    augment Function.prototype to add in a curry method

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
 import './curry'
+
+/**
+ * Calculates the sum of two numbers.
+ * @method add
+ * @param  {Number} x - The augend.
+ * @param  {Number} y - The addend.
+ */
+function add(x, y) {
+  return x + y
+}
+
+const add1 = add.curry(1)
+add1(5) // 6
---
+++ src/curry.js
@@ -0,0 +1,26 @@
+/**
+ * Definition of the curry method.
+ */
+
+/**
+ * Augments Function.prototype to add in a curry method.
+ * @method
+ * @return {Function} - A function that already has one argument passed but
+ * needs the rest of its arguments.
+ */
+Function.prototype.curry = function () {
+  /**
+   * The function's arguments converted into an array.
+   * @type {Array}
+   */
+  const args = [...arguments]
+  /**
+   * The function that curry is being called upon.
+   * @type {Function}
+   */
+  const that = this
+
+  return function () {
+    return that.apply(null, args.concat(...arguments))
+  }
+}
